4e7690d4cd6ecb41fcc48b22251009fe8f6f2b86
Patrick Litschel <padi.litschel@web.de>
2023-10-12 00:08:45 +0200

    [add] methods to handle reactions

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
 import json
-import os
 import asyncio
-
 import discord
+import easyVereinAPIHandler as api
 
 with open('config.json', 'r') as f:
     config = json.load(f)
+
+DISCORD_TOKEN = config['discord_token']
 
 
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    async def setup_hook(self) -> None:
-        # create the background task and run it in the background
-        self.loop.create_task(self.my_background_task())
-
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
-    async def my_background_task(self):
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        if payload.message_id == 1161782437798494228:
+            channel = self.get_channel(711943678566072455)
+            await channel.send(f"<@{payload.user_id}> kriegt jetzt Geburtstagswünsche! :)")
+
+    async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
+        if payload.message_id == 1161782437798494228:
+            channel = self.get_channel(711943678566072455)
+            await channel.send(f"<@{payload.user_id}> kriegt jetzt keine Geburtstagswünsche mehr :(")
+
+    async def setup_hook(self):
+        self.loop.create_task(self.congratulate_on_birthdays())
+
+    async def congratulate_on_birthdays(self):
         await self.wait_until_ready()
         channel = self.get_channel(711943678566072455)  # orga-spam
         while not self.is_closed():
-            vorstand = self.get_guild(711943678322540545).get_role(711943678566072337).members  # abteilungsleitung
-            for member in vorstand:
-                await channel.send(f"Happy Birthday <@{member.id}>!")
-            await asyncio.sleep(10)
+            for member in api.get_birthday_members():
+                await channel.send(f"Happy Birthday <@{api.get_discord_id(member)}>!")
+            await asyncio.sleep(86400)  # 24 hours
 
 
 intents = discord.Intents.default()
 intents.members = True
+intents.message_content = True
+intents.reactions = True
 client = MyClient(intents=intents)
-client.run(config['token'])
-
+client.run(DISCORD_TOKEN)
