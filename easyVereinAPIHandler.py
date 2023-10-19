5e751165c90bc8280f7da6eeccedef0d0a6208fe
Patrick Litschel <padi.litschel@web.de>
2023-10-19 22:09:50 +0200

    [add] method to give Mitglied role to new members [add] method to remove Mitglied role from leaving members [chg] several small refactorings/optimizations

--- easyVereinAPIHandler.py
+++ easyVereinAPIHandler.py
@@ -1,5 +1,5 @@
 import json
-from datetime import date
+from datetime import date, datetime, timedelta
 import requests
 
 with open('config.json', 'r') as f:
@@ -9,18 +9,58 @@ API_TOKEN = config['API_token']
 header = {'Authorization': f'Token {API_TOKEN}'}
 
 
+def get_member_joins_and_leaves():
+    res = requests.get(
+        'https://easyverein.com/api/v1.6/member?query={joinDate,resignationDate,customFields}&limit=1000',
+        headers=header)
+    members = _get_all_results(res)
+    joined_member_ids = _get_latest(members, 'joinDate')
+    left_member_ids = _get_latest(members, 'resignationDate')
+    return joined_member_ids, left_member_ids
+
+
 # TODO
 def set_birthday_wishes_checkbox():
     res = requests.patch("https://easyverein.com/api/v1.6/member/1253810/custom-fields/178015180", headers=header)
 
 
-def get_discord_id(member_url):
-    member = requests.get(member_url+"?query={customFields}&limit=1000", headers=header).json()
-    res = requests.get(member['customFields'], headers=header)
+def get_discord_id_if_allowed(member_url):
+    res = requests.get(member_url + "/custom-fields?limit=1000", headers=header)
     custom_fields = _get_all_results(res)
     if _allows_birthday_wishes(custom_fields):
         return _find_discord_id(custom_fields)
     return 0
+
+
+def get_birthday_members():
+    members = _get_members()
+    birthday_members = []
+    for member in members:
+        _convert_to_date(member, 'dateOfBirth')
+
+        birthday = member['dateOfBirth']
+        today = date.today()
+
+        if birthday.month == today.month and birthday.day == today.day:
+            birthday_members.append(member['member'])
+
+    return birthday_members
+
+
+def _get_discord_id(custom_fields_url):
+    res = requests.get(custom_fields_url + "?limit=1000", headers=header)
+    custom_fields = _get_all_results(res)
+    return _find_discord_id(custom_fields)
+
+
+def _get_latest(members, key):
+    results = []
+    for member in members:
+        if member[key] is not None:
+            _convert_to_date(member, key)
+            if date.today() - timedelta(days=1) == member[key]:  # member joined/left yesterday
+                results.append(_get_discord_id(member['customFields']))
+    return results
 
 
 def _allows_birthday_wishes(custom_fields):
@@ -35,21 +75,6 @@ def _find_discord_id(custom_fields):
             return field['value']
 
 
-def get_birthday_members():
-    members = _get_members()
-    birthday_members = []
-    for member in members:
-        _convert_birthday_to_date(member)
-
-        birthday = member['dateOfBirth']
-        today = date.today()
-
-        if birthday.month == today.month and birthday.day == today.day:
-            birthday_members.append(member['member'])
-
-    return birthday_members
-
-
 def _get_members():
     res = requests.get('https://easyverein.com/api/v1.6/contact-details?query={dateOfBirth, member}&limit=1000',
                        headers=header)
@@ -62,10 +87,10 @@ def _get_all_results(res):
     next_page = res.json()['next']
     while next_page:
         res = requests.get(next_page, headers=header)
-        all_results += res.json()['results']
+        all_results.extend(res.json()['results'])
         next_page = res.json()['next']
     return all_results
 
 
-def _convert_birthday_to_date(member):
-    member['dateOfBirth'] = date.fromisoformat(str(member['dateOfBirth']))
+def _convert_to_date(member, field):
+    member[field] = datetime.fromisoformat(str(member[field])).date()
--- main.py
+++ main.py
@@ -29,18 +29,38 @@ class MyClient(discord.Client):
             await channel.send(f"<@{payload.user_id}> kriegt jetzt keine Geburtstagswünsche mehr :(")
 
     async def setup_hook(self):
-        self.loop.create_task(self.congratulate_on_birthdays())
+        self.loop.create_task(self.do_routines())
+
+    async def do_routines(self):
+        await self.wait_until_ready()
+        while not self.is_closed():
+            await self.congratulate_on_birthdays()
+            await self.update_member_roles()
+            await asyncio.sleep(86400)  # 24 hours
+
+    # TODO change discord tag to discord ID for new members
+    # TODO set checkbox if member is not on server yet
+    async def update_member_roles(self):
+        server = self.get_guild(711943678322540545)
+        mitglied = server.get_role(711943678561746970)
+        joined_members, left_members = api.get_member_joins_and_leaves()
+
+        members = [member for member in joined_members if server.get_member(int(member))]
+        for discord_id in members:
+            member = server.get_member(int(discord_id))
+            await member.add_roles(mitglied)
+
+        members = [member for member in left_members if server.get_member(int(member))]
+        for discord_id in members:
+            member = server.get_member(int(discord_id))
+            await member.remove_roles(mitglied)
 
     async def congratulate_on_birthdays(self):
-        await self.wait_until_ready()
         channel = self.get_channel(711943678566072455)  # orga-spam
-        while not self.is_closed():
-            for member in api.get_birthday_members():
-                discord_id = api.get_discord_id(member)
-                if discord_id != 0:
-                    await channel.send(f"Happy Birthday <@{discord_id}>!")
-
-            await asyncio.sleep(86400)  # 24 hours
+        for member in api.get_birthday_members():
+            discord_id = api.get_discord_id_if_allowed(member)
+            if discord_id != 0:
+                await channel.send(f"Happy Birthday <@{discord_id}>!")
 
 
 intents = discord.Intents.default()
